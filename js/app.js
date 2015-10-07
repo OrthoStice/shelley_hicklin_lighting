55232b30697ff86442e5dc1eb579c4f59cc16105
orthostice <shelleyhicklin@hotmail.com>
2015-10-07 16:00:32 +0000

    testing init, nav bugfixes)

--- js/app.js
+++ js/app.js
@@ -8,3 +8,54 @@ $("#nav li").click (function(e){
 
 $("header h1,nav").hide().fadeIn(3500);
 $("#Bio").hide().fadeIn(3500);
+
+//testing out some init stuff
+
+$(document).ready(function(){
+
+	$('#intro').css({'height':(($( window ).innerHeight()))+'px'});
+
+	$('.image-link').magnificPopup({
+	  type: 'image',
+	  gallery:{
+	    enabled:false
+	  }
+	});
+
+	var tw = $('.thumb').width();
+	$('.thumb').css({'height':tw+'px'});
+
+	var sections = $('section')
+	  , nav = $('nav');
+	 
+	$(window).on('scroll', function () {
+	  var cur_pos = $(this).scrollTop();
+	 
+	  sections.each(function() {
+	    var top = $(this).offset().top -32,
+	        bottom = top + $(this).outerHeight();
+	 
+	    if (cur_pos >= top && cur_pos <= bottom) {
+	      nav.find('a').removeClass('active');
+	      sections.removeClass('active');
+	 
+	      $(this).addClass('active');
+	      nav.find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
+	    }
+
+
+
+	  });
+
+	});
+});
+
+$(window).resize(function(){
+	
+	$('#intro').css({'height':(($( window ).innerHeight()))+'px'});
+	
+	var tw = $('.thumb').width();
+	$('.thumb').css({'height':tw+'px'});
+
+});
+
