a88f5caad48270e88a3e15e5b517217d34a4a00e
orthostice <shelleyhicklin@hotmail.com>
2015-10-14 18:43:14 +0000

    trying new scroll

--- js/app.js
+++ js/app.js
@@ -1,13 +1,32 @@
+$(window).scroll(function() {
+    var windscroll = $(window).scrollTop();
+    if (windscroll >= 100) {
+        $('nav').addClass('fixed');
+        $('.wrapper section').each(function(i) {
+            if ($(this).position().top <= windscroll - 100) {
+                $('nav a.active').removeClass('active');
+                $('nav a').eq(i).addClass('active');
+            }
+        });
+
+    } else {
+
+        $('nav').removeClass('fixed');
+        $('nav a.active').removeClass('active');
+        $('nav a:first').addClass('active');
+    }
+
+}).scroll()
+
 //create nav li click event
+// $("#nav li").click (function(e){
+//     console.log($(this).parent().children());
+//     $(this).parent().children().removeClass('active');
+//     $(this).addClass("active"); 
+// });
 
-$("#nav li").click (function(e){
-    console.log($(this).parent().children());
-    $(this).parent().children().removeClass('active');
-    $(this).addClass("active"); 
-});
-
-$("header h1,nav").hide().fadeIn(4000);
-$("#Bio").hide().fadeIn(5000);
+// $("header h1,nav").hide().fadeIn(4000);
+// $("#Bio").hide().fadeIn(5000);
 
 //testing out some init stuff
 
