8bfde2c572eb0d42b30f78e82674f5052f802f10
orthostice <shelleyhicklin@hotmail.com>
2015-10-25 00:28:25 +0000

    WORKING AJAXgit add -A

--- js/slideshow.js
+++ js/slideshow.js
@@ -10,13 +10,11 @@ $(document).ready(function() {
 	var photoSetID = '72157657833064644';
     
     var flickerAPI= 'https://api.flickr.com/services/rest/?format=json&method=flickr.photosets.getPhotos&api_key=' + apiKey + '&photoset_id=' + photoSetID + '&user_id=51428176@N08&jsoncallback=?';
-    var flickerOptions= {
-        format: "json"
-    };
+    var flickerOptions= "photoset";
   
   function displayPhotos(data) {
       var photoHTML = '<ul>';
-      $.each(flickerOptions.photo,function(i,item) {
+      $.each(data.photoset.photo,function(i,item) {
         
         photoHTML += '<li class="grid-25 tablet-grid-50">';
         photoHTML += '<img src="http://farm' + item.farm + '.static.flickr.com/' + item.server + '/' + item.id + '_' + item.secret + '_m.jpg>';
